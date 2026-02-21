0b9310be3f6711af4b5bdabda8c2f4afb3526b65
kev0402 <kevinnbalana@gmail.com>
2026-02-21 13:22:17 +0800

    added a close button in the advert in mobile design

--- src/components/Ads/AdCarouselModal.jsx
+++ src/components/Ads/AdCarouselModal.jsx
@@ -122,16 +122,14 @@ export default function AdCarouselModal() {
                       className="w-100 h-100 adImage"
 
                     />
-                    <div className="adDontShow d-block d-sm-none">
-                      <Button>Button</Button>
-                    </div>
                     {/* <Form.Check
                     type="checkbox"
                     label="Don’t show again"
                     checked={dontShowAgain}
                     onChange={(e) => setDontShowAgain(e.target.checked)}
-                  /> */}
+                    /> */}
                   </div>
+
 
                   <div className="adRight col-md-6 text-white d-flex flex-column justify-content-center text-center p-4">
                     <div
@@ -167,6 +165,11 @@ export default function AdCarouselModal() {
             </Carousel.Item>
           ))}
         </Carousel>
+        <div className="adDontShow">
+          <Button onClick={handleClose} variant="secondary">
+            <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m12 13.4l-4.9 4.9q-.275.275-.7.275t-.7-.275t-.275-.7t.275-.7l4.9-4.9l-4.9-4.9q-.275-.275-.275-.7t.275-.7t.7-.275t.7.275l4.9 4.9l4.9-4.9q.275-.275.7-.275t.7.275t.275.7t-.275.7L13.4 12l4.9 4.9q.275.275.275.7t-.275.7t-.7.275t-.7-.275z"></path></svg>
+          </Button>
+        </div>
       </Modal.Body>
     </Modal>
   );
